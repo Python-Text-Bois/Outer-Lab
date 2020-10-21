93a1b9463f82e1dec731144e0fe41c000a3b9cb2
The emerald emerald <jadielocollazo89@gmail.com>
2020-10-21 22:04:02 +0000

    Finished up main, so cool right?

--- Script/Useful.py
+++ Script/Useful.py
@@ -1,7 +1,9 @@
+#Easy way to produce an UI for input
 def InputMenu(menu,demands):
     print(menu)
     return input(demands+" ")
-    
+
+#overwrites save file with template
 def NewSave():
     temp=open("savetemp.txt")
     temp2=temp.read()
@@ -10,6 +12,7 @@ def NewSave():
     temp.write(temp2)
     temp.close()
 
+#returns contents of specified file
 def Fetch(filename):
     file=open(filename)
     a=file.read()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import os
+os.system("clear")
 print("Welcome to a board game! \nWIP\n")
-#other files
-import Useful as U
-#packages and that
-import random
-import sys
 
+import Script.Useful as U
+import random
+
+#main menu
 I=U.InputMenu("""
 /t\\
 |b|
@@ -13,7 +14,19 @@ Text Based Bois
 Indepentant member project
 ""","[N]ew game\n[I]mport\n\n")
 
+#Overwrites save with template
 if I == "N":
     U.NewSave()
-#else I == "I":
-    #U.InputMenu("","Insert code in here.")
+#overwrites save with input
+elif I == "I":
+    e=U.InputMenu("","Insert code in here. INSERTING SOMETHING ELSE WILL BREAK THE GAME.")
+    l=open("save.txt","w")
+    l.write(e)
+    l.close()
+    del e;del l
+    
+#Game loop
+os.system("clear")
+#to see what goes on after this visit the game.py file
+import Script.game as g
+g.game()
